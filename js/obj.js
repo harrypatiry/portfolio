8bfd1064fab383b34c62d51578494552e5a84eb4
harrypatiry <harrypatiry@gmail.com>
2022-05-17 13:07:02 -0400

    .

--- js/obj.js
+++ js/obj.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector("canvas.webgl");
 const sizes = {
@@ -24,16 +24,17 @@ const plane = new THREE.Mesh(geometry, material);
 plane.rotation.y = 0;
 plane.rotation.x = -11;
 plane.rotation.z = 90;
-plane.position.y = 6;
+plane.position.y = 14;
 plane.position.x = 0;
-plane.position.z = 0;
+plane.position.z = 10;
+plane.scale.set(1,1,1)
 scene.add(plane);
 
 const count = geometry.attributes.position.count;
 // const ambLight = new THREE.AmbientLight(0xffffff, 1);
 const pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
+pointLight.position.x = 40;
+pointLight.position.y = -1;
 pointLight.position.z = 5;
 scene.add( pointLight );
 
@@ -57,16 +58,21 @@ const camera = new THREE.PerspectiveCamera(
   );
   camera.lookAt(0, 0, 0);
   camera.position.z = 5;
-  camera.position.y = -3;
+  camera.position.y = 5;
+  camera.rotation.x = 7;
   scene.add(camera);
   
   const clock = new THREE.Clock();
   
-  new OrbitControls( camera, renderer.domElement );
+  // new OrbitControls( camera, renderer.domElement );
   window.addEventListener("click", (event) => {
     event.preventDefault();
     plane.material.color = new THREE.Color(0xffffff * Math.random());
     plane.material.needsUpdate = true;
+    // pointLight.position.x++;
+    // pointLight.position.y--;
+    // pointLight.position.y++;
+    // pointLight.position.needsUpdate = true;
   });
   
   const animate = () => {
