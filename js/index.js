a3c91999c99256e59c2dcb597058b07473e86ba2
harrypatiry <harrypatiry@gmail.com>
2022-06-07 15:08:27 -0400

    update about

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const mobile = document.querySelector('.mobile-menu');
     const head = document.querySelector('#head');
     const nav = document.querySelector('.nav-links');
-    mobile.addEventListener('click', function() {
+    mobile.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         head.classList.toggle('head-active');
     })
