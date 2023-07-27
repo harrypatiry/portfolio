fe33b76cd2219d9f080fd63483c948f68cb111f8
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-07-26 22:15:46 -0400

    project grid & about text

--- js/index.js
+++ js/index.js
@@ -7,22 +7,3 @@ window.onload = function() {
         head.classList.toggle('head-active');
     })
 }
-
-const buttons = document.querySelectorAll("[data-scroll-button]")
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.scrollButton === "next" ? 1 : -1
-    const items = button
-      .closest("[data-scroll]")
-      .querySelector("[data]")
-
-    const activeItem = items.querySelector("[data-active]")
-    let newIndex = [...items.children].indexOf(activeItem) + offset
-    if (newIndex < 0) newIndex = items.children.length - 1
-    if (newIndex >= items.children.length) newIndex = 0
-
-    items.children[newIndex].dataset.active = true
-    delete activeItem.dataset.active
-  })
-})
